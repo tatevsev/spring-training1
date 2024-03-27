9b8292569e5c62bd9dc7c6a52659e4689233295f
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-03-27 19:59:44 -0400

    Spring MVC MODEL

--- spring-09-MVC-MODEL/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-MODEL/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -29,6 +30,7 @@ public class CarController {
 
     }
 
+    //QUERY PARAMETER
     //localhost:8080/info3?make=Honda&year=2015
     @RequestMapping("/info3")
     public String carInfo3(@RequestParam String make,@RequestParam int year, Model model){
@@ -36,6 +38,16 @@ public class CarController {
         model.addAttribute("make",make);
         model.addAttribute("year",year);
 
+        return "car/car-info";
+
+    }
+
+    //PATH VARIABLE
+    @RequestMapping("/info/{make}/{year}") //localhost:8080/info/honda/2015
+    public String getCarInfo(@PathVariable String make,@PathVariable String year){
+
+        System.out.println(make);
+        System.out.println(year);
         return "car/car-info";
 
     }
