a1628f4e52fe94c7e800aaa193b74059ef534175
tatevsevacheryan <tateviksevacheryan@gmail.com>
2024-03-27 11:27:20 -0400

    MVC Controller

---
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(){
+        return "home.html";
+    }
+
+    @RequestMapping("/ozzy")
+    public String getHomePage2(){
+        return "home.html";
+    }
+
+    @RequestMapping  //nothing
+    public String getHomePage3(){
+        return "home.html";
+    }
+
+    @RequestMapping({"/apple","/orange"})
+    public String getHomePage4(){
+        return "home.html";
+    }
+}
