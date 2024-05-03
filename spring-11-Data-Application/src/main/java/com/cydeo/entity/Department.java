5f871c6d0d217e6844732e0a6afc822d0f83a94a
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-05-03 16:48:05 +0400

    Commits

--- spring-11-Data-Application/src/main/java/com/cydeo/entity/Department.java
+++ spring-11-Data-Application/src/main/java/com/cydeo/entity/Department.java
@@ -2,10 +2,12 @@ package com.cydeo.entity;
 
 import jakarta.persistence.Access;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 
 public class Department extends BaseEntity {
     private String department;
--- spring-11-Data-Application/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-Application/src/main/java/com/cydeo/entity/Employee.java
@@ -1,22 +1,26 @@
 package com.cydeo.entity;
 
+
 import com.cydeo.enums.Gender;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Entity
 @Table(name = "employees")
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-public class Employee extends BaseEntity{
+public class Employee extends BaseEntity {
 
     private String firstName;
     private String lastName;
+    private String email;
     @Column(columnDefinition = "DATE")
-    private LocalDate hireDate ;
+    private LocalDate hireDate;
     @Enumerated(EnumType.STRING)
     private Gender gender;
     private int salary;
+
 }
