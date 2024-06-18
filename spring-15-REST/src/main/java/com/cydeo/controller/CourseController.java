4da5ebee6d4dce6625b6dad10acef9e04982c9d1
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-06-19 02:04:27 +0400

    Spring REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
 
 
