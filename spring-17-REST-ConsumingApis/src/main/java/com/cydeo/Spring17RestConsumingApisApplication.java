2d39dedd9534af6ff81e1120836770340741d2c9
tatevsev <149326385+tatevsev@users.noreply.github.com>
2024-06-19 19:11:43 +0400

    Spring REST

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -3,10 +3,12 @@ package com.cydeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableFeignClients
 public class Spring17RestConsumingApisApplication {
 
 	public static void main(String[] args) {
